944cf110d4ad80818892a7c60bf18b1c3a3c89c1
gmuribe69-design <gmuribe69@gmail.com>
2025-10-02 10:02:39 -0600

    Create QuienesSomos.jsx

---
+++ src/public/src/src/components/src/components/src/pages/src/pages/QuienesSomos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const QuienesSomos = () => {
+  return (
+    <div className="quienes-somos">
+      <h1>Quiénes Somos</h1>
+      <p>Información sobre Nexo Ilación Concomitancia...</p>
+    </div>
+  )
+}
+
+export default QuienesSomos
