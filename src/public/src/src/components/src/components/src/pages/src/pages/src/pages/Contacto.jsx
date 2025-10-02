bbd5d7ffb88ebb9747b3fe7a900eb0da6e75620e
gmuribe69-design <gmuribe69@gmail.com>
2025-10-02 10:03:02 -0600

    Create Contacto.jsx

---
+++ src/public/src/src/components/src/components/src/pages/src/pages/src/pages/Contacto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contacto = () => {
+  return (
+    <div className="contacto">
+      <h1>Contacto</h1>
+      <p>Formulario de contacto...</p>
+    </div>
+  )
+}
+
+export default Contacto
