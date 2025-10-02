af0fec79b712d25043a35b90abd7844f06a4f0db
gmuribe69-design <gmuribe69@gmail.com>
2025-10-02 10:03:25 -0600

    Create Acceso.jsx

---
+++ src/public/src/src/components/src/components/src/pages/src/pages/src/pages/src/pages/Acceso.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Acceso = () => {
+  return (
+    <div className="acceso">
+      <h1>Acceso y Suscripción</h1>
+      <p>Sistema de acceso premium...</p>
+    </div>
+  )
+}
+
+export default Acceso
